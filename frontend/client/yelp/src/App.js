e116313ad5e1c0373d1bc7fe65a0efd4baf15462
oliverclaxton <oliver.claxton66@gmail.com>
2020-10-11 16:11:58 +0200

    added basic routes in react

--- frontend/client/yelp/src/App.js
+++ frontend/client/yelp/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from './routes/Home';
+import Restaurant from './routes/Restaurant';
+import UpdatePage from './routes/UpdatePage';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello app!!</h1>
-    </div>
-  );
-}
+
+const App = () => {
+  return <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+          <Route exact path="/restaurants/:id" component={Restaurant}/>
+        </Switch>
+      </Router>
+    </div>;
+};
 
 export default App;
---
+++ frontend/client/yelp/src/routes/Restaurant.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const estaurant = () => {
+    return (
+        <div>
+            <h1>restaurants</h1>
+        </div>
+    )
+}
+
+export default estaurant
